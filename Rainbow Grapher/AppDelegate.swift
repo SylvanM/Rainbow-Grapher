f4dc3887c5a65b1ddecad5d6d8ea11fb5458929b
SylvanM <alephnulltim@gmail.com>
2021-10-16 01:50:09 -0400

    Initial Commit

---
+++ Rainbow Grapher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rainbow Grapher
+//
+//  Created by Sylvan Martin on 10/16/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
